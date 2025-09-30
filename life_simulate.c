c7e410c910133127b606d7e41de5173c4d3afdee
unknown <okhapka.a@gmail.com>
2025-09-30 17:49:47 +0200

    add functionality for updated map

--- life_simulate.c
+++ life_simulate.c
@@ -15,11 +15,15 @@ typedef struct
   uint32_t y;
 } start_coord_t;
 
+static int loops_counter = 0;
+
 uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_t initial_point_count)
 {
-  uint8_t *upgrated_life = calloc((grid_dim*grid_dim), sizeof(uint8_t));
+  static uint8_t map[4096];
+  uint8_t *buffer = calloc((grid_dim*grid_dim), sizeof(uint8_t));
   if(!upgrated_life)
-  exit(1);
+   exit(1);
+
   int count = 0;
   for (int dx = -1; dx <= 1; dx++)
   {
@@ -60,4 +64,33 @@ int count_neighbour (uint32_t char_grid[grid_dim][grid_dim], int x, int y)
     }
   }
   return count;
+}
+
+uint8_t *get_next_generation(uint8_t[2048][2048], uint8_t grid_dim, uint8_t *buffer)
+{
+  int counte = 0;
+  int cordi = 0; //to convert 2D into 1D buffer index
+  for (int i = 0; i < grid_dim; i++)
+  {
+    for (int j = 0; j < grid_dim; j++)
+    {
+      cordi = (i + j) + (i * (grid_dim - 1)) + grid_dim;
+      counter = count_neighbour(map, grid_dim, i, j);
+      if (map[i][j] == 0)
+      {
+        if(counter == 3)
+          buffer[cordi] = 1;
+        else
+          buffer[cordi] = 0;
+      }
+      else
+      {
+      if(counter == 2 || counter == 3)
+        buffer[cordi] = 1;
+      else
+        buffer[cordi] = 0;
+      }
+    }
+  }
+  return buffer;
 }
